194bda2ded01e6eabb82ffbc597377497d9052d1
Gabriel Lewis <benbowgl1@gmail.com>
2016-02-24 18:44:44 -0500

    Initial Commit

---
+++ TorpedoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TorpedoApp
+//
+//  Created by Gabriel Benbow on 2/24/16.
+//  Copyright © 2016 Gabriel Benbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
